b73ac1112af3222c0822e79148f1013e6e601d54
LuizinhoGustavo <lggustavo329@gmail.com>
2025-04-16 07:50:42 -0300

    feat: operação de potência

---
+++ main.py
@@ -0,0 +1,67 @@
+# Operações
+
+def soma(a, b):
+    res = a + b
+    print(f'O resultado é {res:.8f}')
+    return res
+
+def sub(a, b):
+    res = a - b
+    print(f'O resultado é {res:.8f}')
+    return res
+
+def mult(a, b):
+    res = a * b
+    print(f'O resultado é {res:.8f}')
+    return res
+
+def div(a, b):
+    res = a / b
+    print(f'O resultado é {res:.8f}')
+    return res
+
+def power(a, b):
+    i = 1
+    res = a
+    while i < b:
+        print(res)
+        res *= a
+        i += 1
+    print(f'O resultado é {res:.8f}')
+    return res
+    
+
+print("CALCULADORA MUITO FODA DO PROFESSOR MATEUS COELHO 💋 <3\n"
+      "---------------------------------------------------\n"
+      "Digite o código da operação que deseja realizar:\n\n"
+      "Cod 1 - Soma     | Cod 2 - Sub\n"
+      "Cod 3 - Mult     | Cod 4 - Div\n"
+      "Cod 5 - Power    | Cod 6 - Root\n"
+      "Cod 7 - Fat      | Cod 8 - Ln\n"
+      "Cod 9 - Log      | Cod 10 - Seno\n"
+      "Cod 11 - Cosseno | Cod 12 - Tan\n"
+      "Cod 13 - ArcSen  | Cod 14 - Arccos\n"
+      "Cod 15 - Arctan  |\n"
+      "\n"
+      "Cod 16 - STORE   | Cod 17 - Clear\n"
+      "Cod -1 - Exit \n")
+
+cod = ""
+
+while(cod != '-1'):
+    cod = input('Escolha uma operação: ')
+    
+    if(cod != '-1'):
+        a = float(input("Digite o valor do operante A: "))
+        b = float(input("Digite o valor do operante B: "))
+        
+        if cod == "1":
+            soma(a, b)
+        elif cod == "2":
+            sub(a, b)
+        elif cod == "3":
+            mult(a, b)
+        elif cod == "4":
+            div(a, b)
+        elif cod == "5":
+            power(a, b)
